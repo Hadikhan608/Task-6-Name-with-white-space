a59bcb3c2bc19fc65ae3cd58d6f11504bf6bfbf1
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 22:54:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//store a person's name and store a white space character at the beginning and end of the name. make sure you use each character combination,
+// "\t"(tab) and "\n"(next line)
+var Namewithwhitespace = "\t\n Hadi \t\n";
+console.log(Namewithwhitespace);
+var Namewithoutwhitespace = Namewithwhitespace.trim();
+console.log(Namewithoutwhitespace);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//store a person's name and store a white space character at the beginning and end of the name. make sure you use each character combination,
+// "\t"(tab) and "\n"(next line)
+let Namewithwhitespace:string = "\t\n Hadi \t\n";
+console.log(Namewithwhitespace);
+let Namewithoutwhitespace:string = Namewithwhitespace.trim();
+console.log(Namewithoutwhitespace);
